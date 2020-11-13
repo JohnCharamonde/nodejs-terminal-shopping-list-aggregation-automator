8b3e190eb1c97502e9cbbbee3afc424876c72bf2
JohnCharamonde <john.charamonde@gmail.com>
2020-11-13 16:54:20 -0500

    Updates to formatting and organization of list

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
-console.log( "You're going to need: \n - 1.5 lbs chicken breast, \n - 10 oz. barbeque sauce, \n - 1 package frozen vegetables" );
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const ingredients = {
+    'chicken breast': [8, 'oz.', 'meat/fish'],
+    'barbeque sauce': [3, 'oz.', 'sauce'],
+    'low fat chicken marinade': [4, 'oz.', 'sauce'],
+    'long grain wild rice': [6, 'oz.', 'carb'],
+    'chicken broth': [8, 'oz.', 'sauce'],
+    'frozen mixed stir-fry vegetables': [100, 'grams', 'vegetables'],
+    'no soy, light sodium soy sauce': [1, 'splash', 'sauce'],
+    'salt': ['to taste', '', 'seasoning'],
+    'pepper': ['to taste', '', 'seasoning'],
+    'minced ginger': [1, 'oz.', 'seasoning'],
+    'minced garlic': [1, 'oz.', 'seasoning'],
+    'yogurt': [6, 'oz', 'snack']
+}
+
+const list = `You're going to need:\n${
+  Object.keys(ingredients).reduce((acc, ingredient) => {
+      acc += `- ${ingredients[ingredient][0]} ${ingredients[ingredient][1]} ${ingredient}\n`
+      return acc;
+      }, '')}
+`.trimEnd();
+
+console.log(list)
