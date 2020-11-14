8dae0732233983e892c6cba600ee3a4845fd6343
JohnCharamonde <john.charamonde@gmail.com>
2020-11-14 16:09:01 -0500

    Organization function nearly complete

--- bin/index.js
+++ bin/index.js
@@ -4,37 +4,39 @@ const chalk = require("chalk");
 const boxen = require("boxen");
 
 const ingredients = {
-  'chicken breast': [8, 'oz.', 'meat, poulty, fish, and eggs'],
-  'barbeque sauce': [3, 'oz.', 'condiments and spices'],
-  'low fat chicken marinade': [4, 'oz.', 'condiments and spices'],
-  'long grain wild rice': [6, 'oz.', 'bread and grains'],
-  'chicken broth': [8, 'oz.', 'condiments and spices'],
-  'frozen mixed stir-fry vegetables': [100, 'grams', 'fruits and vegetables'],
-  'no soy, light sodium soy sauce': [1, 'splash', 'condiments and spices'],
-  'salt': ['to taste', '', 'condiments and spices'],
-  'pepper': ['to taste', '', 'condiments and spices'],
-  'minced ginger': [1, 'oz.', 'condiments and spices'],
-  'minced garlic': [1, 'oz.', 'condiments and spices'],
-  'chocolate chips': [300, 'grams', 'sweets'],
-  'coffee grinds': [25, 'grams', 'condiments and spices'],
-  'sugar': [100, 'grams', 'condiments and spices'],
-  'flour': [400, 'grams', 'condiments and spices'],
-  'vanilla ice-cream': [1, 'quart', 'sweets'],
-  'unslated butter': [1, 'stick', 'fats']
-};
-
-
-const categorizedIngredients = Object.keys(ingredients).reduce((acc, ingredient) => {
-    const ingredientCategory = ingredients[ingredient[2]];
-    acc[ingredients[ingredientCategory]] ? acc[ingredientCategory].push(ingredient) : acc[ingredientCategory] = [ingredient];
-    return acc;
-  }, {});
-
-// const list = `You're going to need:\n${
-//   Object.keys(ingredients).reduce((acc, ingredient) => {
-//       acc += `- ${ingredients[ingredient][0]} ${ingredients[ingredient][1]} ${ingredient}\n`
-//       return acc;
-//       }, '')}
-// `.trimEnd();
-
-console.log(categorizedIngredients)
+    'chicken breast': [8, 'oz.', 'meat, poulty, fish, and eggs'],
+    'barbeque sauce': [3, 'oz.', 'condiments and spices'],
+    'low fat chicken marinade': [4, 'oz.', 'condiments and spices'],
+    'long grain wild rice': [6, 'oz.', 'bread and grains'],
+    'chicken broth': [8, 'oz.', 'condiments and spices'],
+    'frozen mixed stir-fry vegetables': [100, 'grams', 'fruits and vegetables'],
+    'no soy, light sodium soy sauce': [1, 'splash', 'condiments and spices'],
+    'salt': ['to taste', '', 'condiments and spices'],
+    'pepper': ['to taste', '', 'condiments and spices'],
+    'minced ginger': [1, 'oz.', 'condiments and spices'],
+    'minced garlic': [1, 'oz.', 'Condiments and Spices'],
+    'chocolate chips': [300, 'grams', 'Sweets'],
+    'coffee grinds': [25, 'grams', 'Condiments and Spices'],
+    'sugar': [100, 'grams', 'Condiments and Spices'],
+    'flour': [400, 'grams', 'Condiments and Spices'],
+    'vanilla ice-cream': [1, 'quart', 'Sweets'],
+    'unslated butter': [1, 'stick', 'Fats']
+  };
+  
+  
+  const categorizedIngredients = Object.keys(ingredients).reduce((acc, ingredient) => {
+      const ingredientCategory = ingredients[ingredient][2];
+      acc[ingredientCategory] ? acc[ingredientCategory].push(ingredient) : acc[ingredientCategory] = [ingredient];
+      return acc;
+    }, {});
+  
+  const list = `You're going to need:\n${
+    Object.keys(categorizedIngredients).reduce((fullList, category) => {
+      fullList += `${category} \n ${categorizedIngredients[category].reduce((subList, ingredient) => {
+        subList += `- ${ingredient} \n`
+        return subList
+      }, '')}`
+      return fullList;
+    }, '')}`.trimEnd();
+  
+  console.log(list)
